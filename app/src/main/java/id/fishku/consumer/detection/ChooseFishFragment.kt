ecb8c12386771059858996a45a8e050165da0dc1
AxsevSutrisna <asutrisna84@gmail.com>
2023-11-17 21:38:56 +0700

    update
    perubahan
    1. Membuat Activity baru buat informasi ikan dan jenis olahan ikan
    2. Rv buat olahan ikan belum bisa berjalan karena kayaknya pake cardView
    3. nambahin asset dan menu
    4. ubah toolbar di deteksi ikan buat akses activity informasti gizi dan olahan ikan

--- app/src/main/java/id/fishku/consumer/detection/ChooseFishFragment.kt
+++ app/src/main/java/id/fishku/consumer/detection/ChooseFishFragment.kt
@@ -1,21 +1,27 @@
 package id.fishku.consumer.detection
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import id.fishku.consumer.R
 import id.fishku.consumer.core.data.Resource
 import id.fishku.consumer.core.domain.model.FishType
 import id.fishku.consumer.core.ui.FishTypeAdapter
 import id.fishku.consumer.core.utils.showMessage
 import id.fishku.consumer.databinding.FragmentChooseFishBinding
+import id.fishku.consumer.fishinformation.FishInformationActivity
 
 @AndroidEntryPoint
-class ChooseFishFragment : Fragment() {
+class ChooseFishFragment : Fragment(), Toolbar.OnMenuItemClickListener,
+    androidx.appcompat.widget.Toolbar.OnMenuItemClickListener {
 
     private var _binding: FragmentChooseFishBinding? = null
     private val binding get() = _binding
@@ -30,12 +36,14 @@ class ChooseFishFragment : Fragment() {
     ): View? {
         _binding = FragmentChooseFishBinding.inflate(inflater, container, false)
         return binding?.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         setupData()
+        setupAction()
     }
 
     fun setPartOfFish(fishName: String?, fishPart: String?) {
@@ -60,6 +68,7 @@ class ChooseFishFragment : Fragment() {
                         tvErrorSearch.visibility = View.GONE
                     }
                 }
+
                 is Resource.Success -> {
                     binding?.loadingSearch?.visibility = View.GONE
                     if (!it.data.isNullOrEmpty()) {
@@ -70,6 +79,7 @@ class ChooseFishFragment : Fragment() {
                         isEmptyResult(true)
                     }
                 }
+
                 is Resource.Error -> {
                     binding?.apply {
                         loadingSearch.visibility = View.GONE
@@ -126,8 +136,27 @@ class ChooseFishFragment : Fragment() {
         _binding = null
     }
 
+    private fun setupAction() {
+        binding?.toolbarChooseFish?.setOnMenuItemClickListener(this)
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.btn_fishpedia -> {
+                val settingIntent = Intent(requireContext(), FishInformationActivity::class.java)
+                startActivity(settingIntent)
+
+                return true
+            }
+
+            else -> false
+        }
+    }
+
     companion object {
         const val FISH_NAME = "fish_name"
         const val FISH_PHOTO = "fish_photo"
     }
+
+
 }
---
+++ app/src/main/java/id/fishku/consumer/fishinformation/FishInformationActivity.kt
@@ -0,0 +1,23 @@
+package id.fishku.consumer.fishinformation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.fishku.consumer.R
+import id.fishku.consumer.databinding.ActivityFishInformationBinding
+
+class FishInformationActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityFishInformationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFishInformationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        hideActionBar()
+    }
+
+    private fun hideActionBar() {
+        supportActionBar?.hide()
+    }
+}
