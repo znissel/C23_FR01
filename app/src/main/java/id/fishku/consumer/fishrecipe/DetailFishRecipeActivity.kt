7d572b18d6b71bbc5ba57ba311d648fadee8a895
AxsevSutrisna <asutrisna84@gmail.com>
2023-12-01 22:26:53 +0700

    perbaikan bug dan function penambahan asset ikan dan gizi ikan coba set location

---
+++ app/src/main/java/id/fishku/consumer/fishrecipe/DetailFishRecipeActivity.kt
@@ -0,0 +1,12 @@
+package id.fishku.consumer.fishrecipe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.fishku.consumer.R
+
+class DetailFishRecipeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_fish_recipe)
+    }
+}
--- app/src/main/java/id/fishku/consumer/fishrecipe/FishRecipeAdapter.kt
+++ app/src/main/java/id/fishku/consumer/fishrecipe/FishRecipeAdapter.kt
@@ -1,13 +1,11 @@
 package id.fishku.consumer.fishrecipe
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import id.fishku.consumer.core.R
+import id.fishku.consumer.core.databinding.ItemRecipeBinding
 import id.fishku.consumer.model.FishRecipe
 
 class FishRecipeAdapter(private val onItemClick: (String) -> Unit) :
@@ -15,9 +13,26 @@ class FishRecipeAdapter(private val onItemClick: (String) -> Unit) :
 
     private var recipeList: List<FishRecipe> = emptyList()
 
-    class RecipeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val recipeImageView: ImageView = itemView.findViewById(R.id.recipe_image)
-        val recipeNameTextView: TextView = itemView.findViewById(R.id.fish_recipe_name)
+    inner class RecipeViewHolder(private val binding: ItemRecipeBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(recipe: FishRecipe) {
+            binding.apply {
+                Glide.with(recipeImage.context)
+                    .load(recipe.photoRecipeUrl)
+                    .into(recipeImage)
+
+                fishRecipeName.text = recipe.recipeName
+            }
+            binding.root.setOnClickListener {
+                onItemClick.invoke(recipe.id)
+                Toast.makeText(
+                    binding.root.context,
+                    "Kamu Memilih Resep " + recipe.recipeName,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 
     fun setRecipes(recipes: List<FishRecipe>) {
@@ -30,25 +45,63 @@ class FishRecipeAdapter(private val onItemClick: (String) -> Unit) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_recipe, parent, false)
-        return RecipeViewHolder(view)
+        val binding = ItemRecipeBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return RecipeViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
         val recipe = recipeList[position]
-
-        Glide.with(holder.recipeImageView.context)
-            .load(recipe.photoRecipeUrl)
-            .into(holder.recipeImageView)
-
-        holder.recipeNameTextView.text = recipe.recipeName
-
-        holder.itemView.setOnClickListener {
-            onItemClick.invoke(recipe.id)
-        }
+        holder.bind(recipe)
     }
 
     override fun getItemCount(): Int {
         return recipeList.size
     }
 }
+
+//class FishRecipeAdapter(private val onItemClick: (String) -> Unit) :
+//    RecyclerView.Adapter<FishRecipeAdapter.RecipeViewHolder>() {
+//
+//    private var recipeList: List<FishRecipe> = emptyList()
+//
+//    class RecipeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//        val recipeImageView: ImageView = itemView.findViewById(R.id.recipe_image)
+//        val recipeNameTextView: TextView = itemView.findViewById(R.id.fish_recipe_name)
+//    }
+//
+//    fun setRecipes(recipes: List<FishRecipe>) {
+//        recipeList = recipes
+//        notifyDataSetChanged()
+//    }
+//
+//    fun getRecipeById(recipeId: String): FishRecipe? {
+//        return recipeList.find { it.id == recipeId }
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+//        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_recipe, parent, false)
+//        return RecipeViewHolder(view)
+//    }
+//
+//    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+//        val recipe = recipeList[position]
+//
+//        Glide.with(holder.recipeImageView.context)
+//            .load(recipe.photoRecipeUrl)
+//            .into(holder.recipeImageView)
+//
+//        holder.recipeNameTextView.text = recipe.recipeName
+//
+//        holder.itemView.setOnClickListener {
+//            onItemClick.invoke(recipe.id)
+//        }
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return recipeList.size
+//    }
+//}
